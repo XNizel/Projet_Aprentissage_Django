27343237f8d690252f68a2b98421c4db667b14d0
Bastien POINTIS <pointis.bastien@live.fr>
2022-12-10 18:47:55 +0100

    Ajout des liens contact et about a la menubar

--- merchex/merchex/urls.py
+++ merchex/merchex/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('bands/', views.band_list, name='band-list'),
     path('bands/<int:id>/', views.band_detail, name='band-detail'),
-    path('about-us/', views.about),
+    path('about-us/', views.about, name='about'),
     path('listings/', views.listings_list, name='listing-list'),
     path('listings/<int:id>/', views.listings_detail, name='listing-detail'),
     path('contact/', views.contact_us, name='contact'),
